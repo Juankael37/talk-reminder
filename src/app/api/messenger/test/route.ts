aaa96604ee489dc07d39608aeae36fd614f3541f
Juankael37 <juankael37@gmail.com>
2026-04-23 15:58:03 +0800

    Simple test

--- src/app/api/messenger/test/route.ts
+++ src/app/api/messenger/test/route.ts
@@ -1,13 +1,5 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const pageToken = process.env.MESSENGER_PAGE_ACCESS_TOKEN
-  
-  // Get conversations with participants
-  const convRes = await fetch(
-    `https://graph.facebook.com/v21.0/me/conversations?access_token=${pageToken}&limit=1&fields=participants,message_count`
-  )
-  const convData = await convRes.json()
-  
-  return NextResponse.json(convData)
+  return NextResponse.json({ test: 'works', time: new Date().toISOString() })
 }
